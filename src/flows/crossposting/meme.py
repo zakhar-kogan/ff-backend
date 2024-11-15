4aa655d27b38fa36d6d1921689a8e68ad72dedcd
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2024-11-16 01:20:30 +0900

    hot fix html link in tgchannelru

--- src/flows/crossposting/meme.py
+++ src/flows/crossposting/meme.py
@@ -56,7 +56,7 @@ def _get_ru_caption_for_crossposting_meme(meme: MemeData, channel: Channel) -> s
     # caption = escape(meme.caption, quote=False) if meme.caption else ""
     # text = caption + "\n\n" + referral_html
 
-    text = cta + ": " + f"""<a href="{ref_link}>🍔🍔🍔</a>"""
+    text = cta + ": " + f"""<a href="{ref_link}">🍔🍔🍔</a>"""
 
     return text
 
