4dbd5bc58b97fb47ad7d1f4f277f18af12b530a0
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2024-11-16 01:52:58 +0900

    iterate or new prefect 3 deployments

--- flow_deployments/crossposting.py
+++ flow_deployments/crossposting.py
@@ -1,24 +1,21 @@
 from prefect.client.schemas.schedules import CronSchedule
 
-from src.config import settings
 from src.flows.crossposting.meme import (
     post_meme_to_tgchannelen,
     post_meme_to_tgchannelru,
 )
 
-deployment_crossposting_tgchannelen = post_meme_to_tgchannelen.deploy(
+deployment_crossposting_tgchannelen = post_meme_to_tgchannelen.serve(
     name="post_meme_to_tgchannelen",
     schedules=[CronSchedule(cron="40 8,10,14,18,20 * * *", timezone="Europe/Moscow")],
-    work_pool_name=settings.ENVIRONMENT,
 )
 
 deployment_crossposting_tgchannelen.apply()
 
 
-deployment_crossposting_tgchannelru = post_meme_to_tgchannelru.deploy(
+deployment_crossposting_tgchannelru = post_meme_to_tgchannelru.serve(
     name="post_meme_to_tgchannelru",
     schedules=[CronSchedule(cron="20 8,10,12,14 * * *", timezone="Europe/Moscow")],
-    work_pool_name=settings.ENVIRONMENT,
 )
 
 deployment_crossposting_tgchannelru.apply()
