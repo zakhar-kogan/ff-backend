1151e78318b3b62f4c8c83afbb85bc2641426804
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2024-11-16 01:45:01 +0900

    add deployment name

--- flow_deployments/crossposting.py
+++ flow_deployments/crossposting.py
@@ -7,6 +7,7 @@ from src.flows.crossposting.meme import (
 )
 
 deployment_crossposting_tgchannelen = post_meme_to_tgchannelen.deploy(
+    name="post_meme_to_tgchannelen",
     schedules=[CronSchedule(cron="40 8,10,14,18,20 * * *", timezone="Europe/Moscow")],
     work_pool_name=settings.ENVIRONMENT,
 )
@@ -15,6 +16,7 @@ deployment_crossposting_tgchannelen.apply()
 
 
 deployment_crossposting_tgchannelru = post_meme_to_tgchannelru.deploy(
+    name="post_meme_to_tgchannelru",
     schedules=[CronSchedule(cron="20 8,10,12,14 * * *", timezone="Europe/Moscow")],
     work_pool_name=settings.ENVIRONMENT,
 )
