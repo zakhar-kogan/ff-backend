91646f18655ceca08fe6e32954854cf1905ef186
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2024-11-16 01:34:54 +0900

    update prefect

--- flow_deployments/crossposting.py
+++ flow_deployments/crossposting.py
@@ -1,5 +1,4 @@
 from prefect.client.schemas.schedules import CronSchedule
-from prefect.deployments import Deployment
 
 from src.config import settings
 from src.flows.crossposting.meme import (
@@ -7,9 +6,7 @@ from src.flows.crossposting.meme import (
     post_meme_to_tgchannelru,
 )
 
-deployment_crossposting_tgchannelen = Deployment.build_from_flow(
-    flow=post_meme_to_tgchannelen,
-    name="post_meme_to_tgchannelen",
+deployment_crossposting_tgchannelen = post_meme_to_tgchannelen.deploy(
     schedules=[CronSchedule(cron="40 8,10,14,18,20 * * *", timezone="Europe/Moscow")],
     work_pool_name=settings.ENVIRONMENT,
 )
@@ -17,9 +14,7 @@ deployment_crossposting_tgchannelen = Deployment.build_from_flow(
 deployment_crossposting_tgchannelen.apply()
 
 
-deployment_crossposting_tgchannelru = Deployment.build_from_flow(
-    flow=post_meme_to_tgchannelru,
-    name="post_meme_to_tgchannelru",
+deployment_crossposting_tgchannelru = post_meme_to_tgchannelru.deploy(
     schedules=[CronSchedule(cron="20 8,10,12,14 * * *", timezone="Europe/Moscow")],
     work_pool_name=settings.ENVIRONMENT,
 )
